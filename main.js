cfcdcc21fdbc81a66e912ffdf9e7921f4beb94e7
ClumsyPenguin <seppe318@gmail.com>
2022-04-14 22:06:04 +0200

    added basic info

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import "./style.css";
+const toggle = document.querySelector("#toggle");
+toggle.addEventListener("click", modeSwitch);
+
+let isLight = true;
+
+function modeSwitch() {
+  isLight = !isLight;
+  let root = document.body;
+  toggle.innerText = isLight ? "🌞" : "🌚";
+  root.classList.toggle("lightMode");
+}
